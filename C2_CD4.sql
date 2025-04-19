bd36177bf15fe6f0fcdddd5253c51f83bf0b3047
Carlos Gimenez Berral <carlos.gimenez@wolterskluwer.com>
2025-04-19 11:28:25 +0200

    c2_cd4

--- C2_CD4.sql
+++ C2_CD4.sql
@@ -1,32 +1,21 @@
-USE master
-GO
-
--- Eliminar base de datos si existe
-IF DB_ID('VENTAS') IS NOT NULL
-	DROP DATABASE VENTAS
-GO
-
--- Crear base de datos en /var/opt/mssql/data/VENTAS (Docker) con especifiacion de archivo primario y de transacciones
-CREATE DATABASE VENTAS
-	ON PRIMARY (
-	NAME = 'VENTAS_PRI',
-	FILENAME = '/var/opt/mssql/data/VENTAS/VENTAS.mdf',
-	SIZE = 50MB,
-	MAXSIZE = 150MB,
-	FILEGROWTH = 20%
-)
-LOG ON(
-	NAME = 'VENTAS_LOG',
-	FILENAME = '/var/opt/mssql/data/VENTAS/VENTAS.ldf',
-	SIZE = 5MB,
-	MAXSIZE = 50MB,
+-- Modificar base de datos en /var/opt/mssql/data/VENTAS (Docker) añadiendo 3 ficheros secundarios NDF
+ALTER DATABASE VENTAS
+	ADD FILE (
+	NAME = 'VENTAS_SEC1',
+	FILENAME = '/var/opt/mssql/data/VENTAS/VENTAS_SEC1.mdf',
+	SIZE = 10MB,
+	MAXSIZE = 500MB,
 	FILEGROWTH = 10%
 )
 GO
-
--- Visualizar base de datos en un listado
-SELECT * FROM SYS.sysdatabases S
-	WHERE S.name = 'VENTAS'
+ALTER DATABASE VENTAS
+	ADD FILE (
+	NAME = 'VENTAS_SEC2',
+	FILENAME = '/var/opt/mssql/data/VENTAS/VENTAS_SEC1.mdf',
+	SIZE = 10MB,
+	MAXSIZE = 500MB,
+	FILEGROWTH = 10%
+)
 GO
 
 -- Visualizar archivos *..DF
