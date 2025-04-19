5951868e6386577f5915e95c945483545049b43e
Carlos Gimenez Berral <carlos.gimenez@wolterskluwer.com>
2025-04-19 10:59:51 +0200

    c2_cd2

---
+++ C2_CD2.sql
@@ -0,0 +1,28 @@
+USE master
+GO
+
+-- Eliminar base de datos si existe
+IF DB_ID('VENTAS') IS NOT NULL
+	DROP DATABASE VENTAS
+GO
+
+-- Crear base de datos en /var/opt/mssql/data/VENTAS (Docker) con especifiacion de archivo maestro
+CREATE DATABASE VENTAS
+ON PRIMARY (
+NAME = 'VENTAS',
+FILENAME = '/var/opt/mssql/data/VENTAS/VENTAS.mdf',
+SIZE = 50MB,
+MAXSIZE = 150MB,
+FILEGROWTH = 20%
+)
+GO
+
+-- Visualizar base de datos en un listado
+SELECT * FROM SYS.sysdatabases S
+WHERE S.name = 'VENTAS'
+GO
+
+-- Visualizar archivos *..DF
+SP_HELPDB VENTAS
+GO
+
